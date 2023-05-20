255a7f8f40f5fd2096d550656c4fe9d8a660d411
DESKTOP-HLPGO6C\owner <juster0706@gmail.com>
2023-05-20 22:55:12 +0900

    FIX commit  = {                            서버가 구동될 수 있도록 잡다한 코드들을 주석처리 했습니다!                              }

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 const express = require("express");
 const app = express();
-const cookieParser = require("cookie-parser");
-const http = require("http");
+// const cookieParser = require("cookie-parser");
+// const http = require("http");
 
-const cors = require("cors");
-const { host } = require("./config/config");
-const port = host.port;
+// const cors = require("cors");
+// const { host } = require("./config/config");
+const port = 3000;
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(cookieParser());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+// app.use(cookieParser());
 
 
 
-// cors
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-    ],
-    credentials: "true",
-    // cors options
-  })
-);
+// // cors
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:3000",
+//     ],
+//     credentials: "true",
+//     // cors options
+//   })
+// );
 
-// router
-const apiMainRouter = require("./routes/index");
-app.use("/api", [apiMainRouter]);
+// // router
+// const apiMainRouter = require("./routes/index");
+// app.use("/api", [apiMainRouter]);
 
 app.listen(port, () => {
   console.log(`running http://localhost:${port}`);
--- models/index.js
+++ models/index.js
@@ -1,3 +1,8 @@
 "use strict";
 
+const path = require("path");
+const Sequelize = require("sequelize");
+
+
+
 module.exports = db;
