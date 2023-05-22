e9ce534547b26d656f356c5be5aa7496489dc746
DESKTOP-HLPGO6C\owner <juster0706@gmail.com>
2023-05-22 23:54:17 +0900

    FEAT commit  = {                               가장 기본적인 post CRUL 초안입니다.                              }

--- (1)controllers/posts.controller.js
+++ (1)controllers/posts.controller.js
@@ -5,6 +5,106 @@ const CommentsService = require("../(2)services/comment.service");
 class PostController {
   commentsService = new CommentsService();
 
+  createPost = async (req, res) => {
+    try {
+      const { userId, nickname } = res.locals.user;
+      const { title, content } = req.body;
+      const { photoUrl } = req;
+
+      if (!title) {
+        return res
+          .status(412)
+          .json({ message: "모든 필드의 값은 필수 값 입니다." });
+      }
+      if (!content) {
+        return res
+          .status(412)
+          .json({ message: "모든 필드의 값은 필수 값 입니다." });
+      }
+
+
+      const postData = {
+        userId,
+        nickname,
+        title,
+        content,
+        photoUrl,
+        like: 0,
+        likeCount:0,
+      };
+      await postService.createPost(postData);
+      return res.status(201).json({ message: "게시글 작성에 성공하였습니다." });
+    } catch (err) {
+      console.error(err);
+      res.status(400).send({ message: "게시글 작성에 실패하였습니다." });
+    }
+  };
+
+  getPosts = async (req, res) => {
+    try {
+      const posts = await postService.getPosts();
+      const result = await Promise.all(
+        posts.map(async (a) => {
+          const b = await this.commentsService.getComments(a.postId);
+          return (a.commentCount = b.length);
+        })
+      );
+
+      if (posts.error) {
+        throw new Error(posts.message);
+      }
+
+      res.json(posts);
+    } catch (err) {
+      console.error(err);
+      res.status(400).send({ message: "게시글 조회에 실패하였습니다." });
+    }
+  };
+
+  getPostById = async (req, res) => {
+    try {
+      const { postId } = req.params;
+      const post = await postService.getPostById(postId);
+      const comments = await this.commentsService.getComments(postId);
+      res.json({ data: post, commentsCount: comments.length });
+    } catch (err) {
+      console.error(err);
+      res.status(400).send({ message: "게시글 조회에 실패하였습니다." });
+    }
+  };
+
+  updatePost = async (req, res) => {
+    try {
+      const { userId } = res.locals.user;
+      const { postId } = req.params;
+      const { title, content } = req.body;
+      const { photoUrl } = req;
+      await postService.updatePost(
+        userId,
+        postId,
+        title,
+        content,
+        photoUrl
+      );
+      res.status(200).json({ message: "게시글을 수정하였습니다." });
+    } catch (err) {
+      console.error(err);
+      res.status(400).send({ errorMessage: "게시글 수정에 실패하였습니다." });
+    }
+  };
+
+  deletePost = async (req, res) => {
+    try {
+      const { userId } = res.locals.user;
+      const { postId } = req.params;
+      await postService.deletePost(userId, postId);
+      res.status(200).json({ message: "게시글을 삭제하였습니다." });
+    } catch (err) {
+      console.error(err);
+      res.status(400).send({ errorMessage: "게시글 삭제에 실패하였습니다." });
+    }
+  };
+
 }
 
 module.exports = PostController;
--- (2)services/posts.service.js
+++ (2)services/posts.service.js
@@ -3,6 +3,84 @@ const { post } = require("../(0)routes");
 const postRepository = new PostRepository();
 
 class PostService {
+    async createPost(postData) {
+        console.log("postData:", postData);
+        return await postRepository.create(postData);
+    }
+    
+    async getPosts() {
+        try {
+        const posts = await postRepository.getPosts();
+        const results = await Promise.all(
+            posts.map(async (item) => {
+                const post = {
+                    postId: item.postId,
+                    userId: item.userId,
+                    nickname: item.nickname,
+                    title: item.title,
+                    content: item.content,
+                    like: item.like,
+                    views: item.views,
+                    createdAt: item.createdAt,
+                    updatedAt: item.updatedAt,
+                    photoUrl: item.photoUrl,
+                    commentCount: null,
+            };
+    
+            return post;
+            })
+        );
+    return results;
+        } catch (error) {
+            return { error: true, message: error.message };
+        }
+    }
+
+
+    getPostById = async (postId) => {
+        const post = await postRepository.findPostById(postId);
+        return post;
+    };
+    
+    updatePost = async (
+        userId,
+        postId,
+        title,
+        content,
+        photoUrl
+    ) => {
+        const post = await postRepository.findPostById(postId);
+
+        if (!title || !content ) {
+            throw new Error("입력 값이 유효하지 않습니다.");
+        }
+
+        if (userId !== post.userId) {
+            throw new Error("게시글 수정 권한이 없습니다.");
+        }
+    
+        await postRepository.updatePostById(
+            postId,
+            title,
+            content,
+            photoUrl
+        );
+    };
+    
+    deletePost = async (userId, postId) => {
+        const post = await postRepository.findPostById(postId);
+    
+        if (!post) {
+            throw new Error("게시글이 존재하지 않습니다.");
+        }
+    
+        if (userId !== post.userId) {
+            throw new Error("게시글 삭제 권한이 없습니다.");
+        }
+    
+        await postRepository.deletePostById(postId);
+    };
+
 }
 
 module.exports = PostService;
