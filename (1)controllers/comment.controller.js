6a433fe763d25d360db8b65e403b6e6a43388ebd
ParkBrianJunSoo <junparkstar@gmail.com>
2023-05-23 21:02:57 +0900

    Feat: comment & childComment CRUD

---
+++ (1)controllers/comment.controller.js
@@ -0,0 +1,116 @@
+const CommentService = require("../(2)services/comment.service.js");
+// const PostsService = require("../(2)services/posts.service.js");
+
+class CommentController {
+    commentService = new CommentService();
+    // postsService = new PostsService();
+
+    // 댓글 생성
+    createComment = async (req, res, next) => {
+        try {
+            const { userId } = res.locals.user;
+            const { postId } = req.params;
+            const { comment } = req.body;
+            const { photoUrl } = req;
+
+            if (!comment) {
+                throw new Error("403/댓글 작성에 실패하였습니다.");
+            }
+
+            // 게시글이 존재하는지 여부 확인
+            // const post = await this.postsService.xxxx(postId); 
+            const post = await this.commentService.findPostById(postId);
+
+            if (!post) {
+                throw new Error("403/게시물이 존재하지 않습니다");
+            }
+
+            await this.commentService.createComment(userId, postId, comment, photoUrl);
+
+            res.status(201).json({ message: "댓글을 작성하였습니다." });
+        } catch (error) {
+            error.failedApi = "댓글 생성";
+            throw error;
+        }
+    };
+
+    // 전체 댓글 조회
+    readComments = async (req, res, next) => {
+        try {
+            const { postId } = req.params;
+
+            const post = await this.commentService.findPostById(postId);
+            if (!post) {
+                throw new Error("403/게시물이 존재하지 않습니다.");
+            }
+
+            // 댓글이 존재하는지 여부 확인
+            const comments = await this.commentService.findCommentsByPostId(postId);
+            if (!comments) {
+                throw new Error("403/댓글이 존재하지 않습니다.");
+            }
+
+            return res.status(200).json({ commentsData: comments });
+        } catch (error) {
+            error.failedApi = "댓글 조회";
+            throw error;
+        }
+    };
+
+    // 댓글 수정
+    fixComment = async (req, res, next) => {
+        try {
+            const { userId } = req.locals.user;
+            const { postId, commentId } = req.params;
+            const { comment } = req.body;
+
+            const post = await this.commentService.findPostById(postId);
+            if (!post) {
+                throw new Error("403/게시물이 존재하지 않습니다.");
+            }
+
+            const comments = await this.commentService.findCommentById(commentId);
+            if (!comments) {
+                throw new Error("403/댓글이 존재하지 않습니다.");
+            }
+
+            await this.commentService.updateComment(userId, postId, commentId, comment);
+
+            return res.status(200).json({ message: "댓글을 수정하였습니다." });
+        } catch (error) {
+            error.failedApi = "댓글 수정";
+            throw error;
+        }
+    }
+
+    // 댓글 삭제
+    deleteComment = async (req, res, next) => {
+        try {
+            const { userId } = res.locals.user;
+            const { postId, commentId } = req.params;
+
+            const post = await this.commentService.findPostById(postId);
+            if (!post) {
+                throw new Error("403/게시물이 존재하지 않습니다.");
+            }
+
+            const comment = await this.commentService.findCommentById(commentId);
+            if (!comment) {
+                throw new Error("403/댓글이 존재하지 않습니다.");
+            }
+
+            if (comment.UserId !== userId) {
+                throw new Error("403/댓글 삭제 권한이 존재하지 않습니다.");
+            }
+
+            await this.commentService.deleteComment(userId, postId, commentId);
+
+            return res.status(200).json({ message: "댓글을 지웠습니다." });
+        } catch (error) {
+            error.failedApi = "댓글 삭제";
+            throw error;
+        }
+    };
+};
+
+module.exports = CommentController;
---
+++ (2)services/childComment.service.js
@@ -0,0 +1,52 @@
+const ChildCommentRepository = require("../(3)repositoryies/childComment.repository.js");
+const CommentRepository = require("../(3)repositories/comment.repository.js");
+
+const { Comments, childComments } = require("../models");
+
+class ChildCommentService {
+    childCommentRepository = new ChildCommentRepository(childComments);
+    commentRepository = new CommentRepository(Comments);
+
+    // 대댓글 생성
+    createChildComment = async (userId, postId, commentId, childComment) => {
+        return await this.childCommentrepository.createChildComment(userId, postId, commentId, childComment);
+    };
+
+    // 대댓글 조회
+    findChildCommentsByCommentId = async (commentId) => {
+        const childComments = await this.childCommentRepository.findChildCommentsByCommentId(commentId);
+
+        const childCommentsWithDetail = await Promise.all(
+            childComments.map(async (childComment) => {
+                const user = await this.commentRepository.findUserById(childComment.UserId);
+
+                return {
+                    childCommentId: childComment.childCommentId,
+                    CommentId: childComment.CommentId,
+                    UserId: childComment.UserId,
+                    postId: childComment.PostId,
+                    childComment: childComment.childComment,
+                    nickname: user.nickname,
+                    profileUrl: user.profileUrl,
+                    createdAt: childComment.createdAt,
+                    updatedAt: childComment.updatedAt
+                }
+            })
+        )
+
+        return childCommentsWithDetail.sort((a, b) => b.createdAt - a.createdAt);
+    };
+
+    // 대댓글 하나 찾기
+    findChildCommentById = async (childCommentId) => {
+        return await this.childCommentRepository.findChildCommentById(childCommentId);
+    };
+
+    // 대댓글 삭제
+    deleteChildComment = async (userId, postId, commentId, childCommentId) => {
+        return await this.childCommentRepository.deleteChildComment(userId, postId, commentId, childCommentId);
+    };
+};
+
+
+module.exports = ChildCommentService;
