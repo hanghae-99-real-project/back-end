ed686667873879f94c8afc6ae951bc6838901e12
DESKTOP-HLPGO6C\owner <juster0706@gmail.com>
2023-05-23 22:11:30 +0900

    REFACTOR(조우상) commit  = {                     경로 수정 및 구조 조정
                                                  }

--- (3)repositories/tokens.repository.js
+++ (3)repositories/tokens.repository.js
@@ -1,31 +1,28 @@
 const { Tokens } = require("../models");
 
 class TokenRepository {
+  // Tokens table에 refresh token 저장
     saveToken = async (userId, refreshToken) => {
-        client.set(refreshToken, userId);
-        const saveToken = await Tokens.create({
-            token: refreshToken,
-            userId: userId,
-        });
-
-        return saveToken;
-    };
-
-    findTokenId = async (authRefreshToken) => {
-        const userId = await new Promise((resolve, reject) => {
-
-            client.get(authRefreshToken, (err, reply) => {
-                if (err) reject(err);
-                resolve(reply);
-        });
+    const saveToken = await Tokens.create({
+        token: refreshToken,
+        userId: userId,
     });
 
-    return userId;
+    return saveToken;
+    };
+
+  // 새로운 Access Token 발급받을때 Refresh Token으로 user_id 가져오기
+    findTokenId = async (authRefreshToken) => {
+        const accessTokenId = await Tokens.findOne({
+        where: { token: authRefreshToken },
+        });
+        const { userId } = accessTokenId;
+
+        return userId;
     };
 
     deleteToken = async (userId) => {
         await Tokens.destroy({ where: { userId } });
-        client.del(userId);
         return;
     };
 }
