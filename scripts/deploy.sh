899c7f71f2c75a5a8104e4fce6c5bbe9ed0e2b3d
DESKTOP-HLPGO6C\owner <juster0706@gmail.com>
2023-05-26 12:34:57 +0900

    no message

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# 현재 실행 중인 프로세스 종료
+pm2 delete 0
 
 # 환경 변수 정의
 BUCKET_NAME="karyl"
@@ -14,3 +16,5 @@ aws s3 cp ./build s3://$BUCKET_NAME/$PROJECT_NAME --recursive
 # CodeDeploy 배포
 aws deploy create-deployment --application-name last_dance --deployment-group-name last_dance --s3-location bucket=$BUCKET_NAME,key=$PROJECT_NAME --region ap-northeast-2
 
+# 새로운 파일로 실행
+pm2 start app.js
