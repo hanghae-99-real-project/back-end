587959f3426dc2a1b343f92a1587875c018872c3
ystar5008@naver.com <ystar5008@naver.com>
2023-05-27 10:51:08 +0900

    feat : (용식) 북마크 추중가

--- (0)routes/index.js
+++ (0)routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const authRouter = require("./users.routes");
 const childCommentRouter = require("./childComment.routes");
 const commentRouter = require("./comment.routes");
-const likeRouter = require("./like.routes");
+const bookmarkRouter = require("./bookmark.routes");
 const mypageRouter = require("./mypage.routes");
 const pooRouter = require("./poo.routes");
 const postRouter = require("./post.routes");
@@ -15,10 +15,12 @@ const searchRouter = require("./search.routes");
 
 router.use("/auth", [authRouter, mypageRouter]);
 
-router.use("/posts", [childCommentRouter, commentRouter, postRouter]);
+router.use("/posts", [childCommentRouter, commentRouter, postRouter, bookmarkRouter]);
 
 router.use("/search", searchRouter);
 
 router.use("/map/poo", pooRouter)
+
+
 
 module.exports = router;
---
+++ (1)controllers/bookmark.controller.js
@@ -0,0 +1,52 @@
+const BookmarkService = require('../(2)services/bookmark.service')
+
+class bookmarksController {
+    bookmarksService = new BookmarkService()
+
+    //북마크 등록
+    postBookmark = async (req, res) => {
+        const { postId } = req.params
+        const { userId } = res.locals.user
+        const createBookmark = await this.bookmarksService.postBookmark(postId, userId)
+
+        return createBookmark
+    }
+
+    // scrapExhibition = async (req, res, next) => {
+    //     try {
+    //       const { userEmail } = res.locals.user;
+
+    //       const { exhibitionId } = await pkIdParamSchema
+    //         .validateAsync(req.params)
+    //         .catch((err) => {
+    //           res.status(400).json({ message: err.message });
+    //           throw Boom.badRequest(err.message);
+    //         });
+
+    //       const updateExhibitionScrap =
+    //         await this.exhibitionService.updateExhibitionScrap(
+    //           userEmail,
+    //           exhibitionId
+    //         );
+
+    //       if (updateExhibitionScrap == "create") {
+    //         return res
+    //           .status(201)
+    //           .json({ message: "해당 전시글을 스크랩 했습니다." });
+    //       } else if (updateExhibitionScrap == "delete") {
+    //         return res
+    //           .status(201)
+    //           .json({ message: "해당 전시글을 스크랩에서 제외했습니다." });
+    //       }
+    //     } catch (error) {
+    //       next(error);
+    //     }
+    //   };
+
+
+
+
+
+}
+
+module.exports = bookmarksController;
