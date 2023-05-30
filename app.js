4f84cea55ebb1e2a5c8fd6c4be698793f7613223
ystar5008@naver.com <ystar5008@naver.com>
2023-05-30 22:01:59 +0900

    fix : 세션에러수정

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const cors = require("cors")
 const dotenv = require('dotenv')
 const cookieParser = require("cookie-parser");
-const session = require('express-session');
-const RedisStore = require("connect-redis").default
+// const session = require('express-session');
+// const RedisStore = require("connect-redis").default
 require("express-async-errors");
 const port = 3000;
 const morgan = require('morgan')
@@ -25,19 +25,19 @@ app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
 
-app.use(session({
-  resave: false,
-  saveUninitialized: true,
-  secret: process.env.COOKIE_SECRET,
-  //세션 데이터를 redis에 저장
-  // Redis에 세션 데이터를 저장하는데 사용할 키의 접두사 설정
-  // 필요에 따라 수정할 수 있습니다.
-  // 세션 암호화를 위한 비밀 키
-  // 변경 사항이 없어도 세션을 항상 저장할지 여부
-  // 초기화되지 않은 세션도 저장할지 여부
-  store: new RedisStore({ client: redisClient, prefix: 'session:' }), // 세션 데이터를 로컬 서버 메모리가 아닌 redis db에 저장하도록 등록
-  cookie: { httpOnly: true, maxAge: 2592000000, secure: false }
-}))
+// app.use(session({
+//   resave: false,
+//   saveUninitialized: true,
+//   secret: process.env.COOKIE_SECRET,
+//   //세션 데이터를 redis에 저장
+//   // Redis에 세션 데이터를 저장하는데 사용할 키의 접두사 설정
+//   // 필요에 따라 수정할 수 있습니다.
+//   // 세션 암호화를 위한 비밀 키
+//   // 변경 사항이 없어도 세션을 항상 저장할지 여부
+//   // 초기화되지 않은 세션도 저장할지 여부
+//   store: new RedisStore({ client: redisClient, prefix: 'session:' }), // 세션 데이터를 로컬 서버 메모리가 아닌 redis db에 저장하도록 등록
+//   cookie: { httpOnly: true, maxAge: 2592000000, secure: false }
+// }))
 app.use("/api", router);
 
 app.use(errorHandler);
