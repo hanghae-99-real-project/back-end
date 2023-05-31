d82fa28927e005b9769497e3b3c52905dac2f75f
DESKTOP-HLPGO6C\owner <juster0706@gmail.com>
2023-06-01 00:27:10 +0900

    fix(조우상): 숼스크립트

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@ sudo \cp -rf /home/ubuntu/temp /home/ubuntu/back-end
 sudo cp /home/ubuntu/.env /home/ubuntu/back-end/
 cd /home/ubuntu/back-end 
 npm install
-pm2 start app.js
+pm2 start /home/ubuntu/back-end/app.js
 
 if [ -d /home/ubuntu/temp ]; then
     sudo rm -rf /home/ubuntu/temp
