aec7d5e2a268b7c7afcb8b64e9d64367f32e6264
DESKTOP-HLPGO6C\owner <juster0706@gmail.com>
2023-06-01 20:05:49 +0900

    디버깅 툴

--- (2)services/users.service.js
+++ (2)services/users.service.js
@@ -151,6 +151,14 @@ class UserService {
 
     // 엑세스 토큰 받아오기
     getTokens = async (authCode) => {
+        console.log(authCode)
+        console.log(querystring.stringify({
+            grant_type: "authorization_code",
+            client_id: process.env.KAKAO_CLIENT_ID,
+            client_secret: process.env.REACT_APP_KAKAO_CLIENT_SECRET,
+            redirect_uri: process.env.KAKAO_REDIRECT_URI,
+            code: authCode,
+        }))
         const response = await axios.post(
             "https://kauth.kakao.com/oauth/token",
             querystring.stringify({
