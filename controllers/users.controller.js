e30cebe76fe133dcdf736d4a35d8463ba09eee9e
DESKTOP-HLPGO6C\owner <juster0706@gmail.com>
2023-06-15 19:17:14 +0900

    fix(조우상): 비밀번호 오류 해결 2ㄴ트

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -110,14 +110,17 @@ class UserController {
       const loginUser = await this.userService.loginUser(phoneNumber);
       const userId = loginUser.userId;
       const passtest = loginUser.password;
-      const passwordMatch = await bcrypt.hash(password, 10);
+      const passwordMatch = await bcrypt.compare(password, loginUser.password);
 
-      if (passtest === passwordMatch ){
+      if (!passwordMatch) {
+        return res.status(400).json({ message: "비밀번호를 확인해주세요" });
+        }
+
       if (!loginUser || !passwordMatch) {
         return res
           .status(401)
           .json({ errorMessage: "닉네임 또는 패스워드를 확인해주세요." });
-      }}else{return res.status(400).json({ message: "비밀번호를 확인해주세요" });}
+      }
 
       await Users.update(
         {
