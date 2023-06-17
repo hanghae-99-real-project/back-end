0539f30569d21b75b91c29b4ee873409b584bba1
ParkBrianJunSoo <junparkstar@gmail.com>
2023-06-17 20:45:13 +0900

    FIX: (박준수) 대댓글 유저아이디 수정

--- services/notification.service.js
+++ services/notification.service.js
@@ -8,30 +8,31 @@ class NotificationService {
     // 특정 유저의 모든 알림 조회
     getNotificationsByUserId = async (userId) => {
         const notifications = await this.notificationRepository.getCommentNotificationsByUserId(userId);
+        const notificationMessage = await Promise.all(
+            notifications.map(async (notification) => {
+                let commentNotification = notification.get({ plain: true });
+                let plainUser = notification.User.get({ plain: true });
 
-        const notificationMessage = notifications.map(notification => {
-            let commentNotification = notification.get({ plain: true });
-            let plainUser = notification.User.get({ plain: true });
+                // ChildCommentId 가 null일 때 댓글만 보여지게 하고, ChildCommentId가 null이 아닐 때 대댓글만 보여지게 알림 설정
+                let message;
+                if (commentNotification.ChildCommentId == null) {
+                    message = `${plainUser.nickname}님이 댓글을 작성하였습니다.`;
+                    delete commentNotification.ChildComment; // 만약 댓글이 있으면 대댓글 삭제
+                } else {
+                    message = `${plainUser.nickname}님이 대댓글을 작성하였습니다.`;
+                    delete commentNotification.Comment;  // 만약 대댓글이 있으면 댓글 삭제
 
-            // ChildCommentId 가 null일 때 댓글만 보여지게 하고, ChildCommentId가 null이 아닐 때 대댓글만 보여지게 알림 설정
-            let message;
-            if (commentNotification.ChildCommentId == null) {
-                message = `${plainUser.nickname}님이 댓글을 작성하였습니다.`;
-                delete commentNotification.ChildComment; // 만약 댓글이 있으면 대댓글 삭제
-            } else {
-                message = `${plainUser.nickname}님이 대댓글을 작성하였습니다.`;
-                delete commentNotification.Comment;  // 만약 대댓글이 있으면 댓글 삭제
-
-            }
-
-            return {
-                ...commentNotification,
-                User: {
-                    ...plainUser,
-                    nickname: message
                 }
-            };
-        });
+
+                return {
+                    ...commentNotification,
+                    User: {
+                        ...plainUser,
+                        nickname: message
+                    }
+                };
+            })
+        )
         return notificationMessage;
 
     };
