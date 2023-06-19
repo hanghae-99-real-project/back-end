b43dd1b2d94e8f30516d607c712da4ee8367d526
ystar5008@naver.com <ystar5008@naver.com>
2023-06-19 23:07:00 +0900

    FIX : (김용식) post라우터 수정

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -5,44 +5,17 @@ const PostController = require("@controllers/posts.controller");
 const postController = new PostController();
 const uploadImage = require("@middlewares/post_s3.js");
 
-router.post(
-    "/",
-    authMiddleware,
-    uploadImage.array("image", 5),
-    async (req, res) => {
-        postController.createPost(req, res);
-    }
-);
+router.post("/", authMiddleware, uploadImage.array("image", 5), postController.createPost)
 
-router.get(
-    "/",
-    authMiddleware,
-    async (req, res) => {
-        postController.getPosts(req, res);
-    });
+router.get("/", authMiddleware, postController.getPosts(req, res));
 
-router.get(
-    "/:postId",
-    postController.getPostById
-);
+router.get("/:postId", postController.getPostById);
 
-router.put(
-    "/:postId",
-    authMiddleware,
-    uploadImage.array("image", 5),
-    postController.updatePost
-);
+router.put("/:postId", authMiddleware, uploadImage.array("image", 5), postController.updatePost);
 
-router.delete(
-    "/:postId",
-    authMiddleware,
-    postController.deletePost
-);
+router.delete("/:postId", authMiddleware, postController.deletePost);
 
-router.post(
-    "/end/:postId",
-    authMiddleware,
-    postController.endPost);
+router.post("/end/:postId", authMiddleware, postController.endPost);
 
 
 module.exports = router;
